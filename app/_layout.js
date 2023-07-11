ca93509e8ee298067fd878822cb6b374cc13ce75
rayleigh420 <nhatduy0409@gmail.com>
2023-07-12 06:49:12 +0700

    Config nativewind for run on web

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot, Stack } from 'expo-router';
+import { NativeWindStyleSheet } from "nativewind";
 
 const queryClient = new QueryClient()
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function HomeLayout() {
   return (
