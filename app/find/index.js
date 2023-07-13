6d80629954ac77f297350873881cb2996fa41b68
rayleigh420 <nhatduy0409@gmail.com>
2023-07-13 08:16:09 +0700

    Fix size icon

--- app/find/index.js
+++ app/find/index.js
@@ -85,7 +85,7 @@ export default function Location() {
 										<Text >Rencent Used</Text>
 									</View>
 								</View>
-								<View className='w-[25px] flex flex-col justify-start items-center pt-[10px] ml-[50px]'>
+								<View className='w-[19px] flex flex-col justify-start items-center pt-[10px] ml-[50px]'>
 									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 										<path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
 									</svg>
@@ -108,10 +108,13 @@ export default function Location() {
 										<Text >Rencent Used</Text>
 									</View>
 								</View>
-								<View className='w-[25px] flex flex-col justify-start items-center pt-[10px] ml-[50px]'>
+								<View className='w-[19px] flex flex-col justify-start items-center pt-[10px] ml-[50px]'>
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
 										<path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z" clipRule="evenodd" />
 									</svg>
+									{/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+										<path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+									</svg> */}
 								</View>
 							</View>
 						</View>
@@ -123,7 +126,6 @@ export default function Location() {
 										<path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
 										<path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
 									</svg>
-
 								</View>
 							</View>
 							<View className="flex flex-row justify-between border-[#EFEFEF] border-b-[1.5px] ml-[10px] pb-[20px]">
