fbb5d72ab8d0ae216657c09a815aef5b38d0071b
rayleigh420 <nhatduy0409@gmail.com>
2023-07-13 20:45:17 +0700

    UI for map screen

--- app/find/index.js
+++ app/find/index.js
@@ -1,6 +1,6 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Entypo, Feather, FontAwesome5, FontAwesome } from '@expo/vector-icons';
@@ -52,25 +52,27 @@ export default function Location() {
 						<Text className="text-[15px] font-bold text-[#166ECF]">View All</Text>
 					</View>
 					<View className="flex flex-col">
-						<View className="flex flex-row mb-[20px]">
-							<View className="flex flex-col justify-start items-center">
-								<View className="w-[25px] p-[3px] mt-[7px] rounded-full bg-[#00B14F] flex justify-center items-center">
-									<Ionicons name="bookmark-sharp" size={17} color="white" />
-								</View>
-							</View>
-							<View className="flex flex-row justify-between border-[#EFEFEF] border-b-[1.5px] ml-[10px] pb-[20px]">
-								<View className="flex flex-col justify-center w-[262px] max-w-[262px]">
-									<Text className='text-[16px] font-bold'>Huỳnh Sơn</Text>
-									<Text className='text-[13px]'>5.34km . Pick up/Drop off Gate, 227 Nguyễn</Text>
-									<View className="bg-[#D9FCDE] p-[2px] pt-[0.5px] mt-[5px] w-[90px] rounded-[7px] font-[600] flex flex-row justify-center">
-										<Text className="text-[13.5px]">Recent Used</Text>
+						<Link href="find/map">
+							<View className="flex flex-row mb-[20px]">
+								<View className="flex flex-col justify-start items-center">
+									<View className="w-[25px] p-[3px] mt-[7px] rounded-full bg-[#00B14F] flex justify-center items-center">
+										<Ionicons name="bookmark-sharp" size={17} color="white" />
 									</View>
 								</View>
-								<View className='w-[19px] flex flex-col justify-start items-center pt-[10px] ml-[30px]'>
-									<Feather name="edit" size={17} color="black" />
+								<View className="flex flex-row justify-between border-[#EFEFEF] border-b-[1.5px] ml-[10px] pb-[20px]">
+									<View className="flex flex-col justify-center w-[262px] max-w-[262px]">
+										<Text className='text-[16px] font-bold'>Huỳnh Sơn</Text>
+										<Text className='text-[13px]'>5.34km . Pick up/Drop off Gate, 227 Nguyễn</Text>
+										<View className="bg-[#D9FCDE] p-[2px] pt-[0.5px] mt-[5px] w-[90px] rounded-[7px] font-[600] flex flex-row justify-center">
+											<Text className="text-[13.5px]">Recent Used</Text>
+										</View>
+									</View>
+									<View className='w-[19px] flex flex-col justify-start items-center pt-[10px] ml-[30px]'>
+										<Feather name="edit" size={17} color="black" />
+									</View>
 								</View>
 							</View>
-						</View>
+						</Link>
 
 						<View className="flex flex-row mb-[20px]">
 							<View className="flex flex-col justify-start items-center">
---
+++ app/find/map.js
@@ -0,0 +1,50 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Entypo, Feather, FontAwesome5, FontAwesome } from '@expo/vector-icons';
+
+export default function Map() {
+    return (
+        <View className="h-full relative bg-white">
+            <Stack.Screen options={{
+                title: "",
+                headerShadowVisible: false,
+                headerStyle: {
+                    backgroundColor: 'transparent',
+                },
+
+                headerTransparent: "true",
+                headerLeft: () => {
+                    return (
+                        <View className="rounded-full min-w-[25px] min-h-[25px] bg-red-500 flex justify-center items-center ml-[15px]">
+                            <Entypo name="chevron-thin-left" size={14} color="black" />
+                        </View>
+                    )
+
+                }
+            }} />
+            <View className="absolute bottom-0 w-full px-[15px] flex flex-col justify-center items-center">
+                <View className="bg-[#F2F2F2] flex flex-row justify-between items-center px-[17px] py-[15px] min-w-[350px] rounded-[5px]">
+                    <View className="flex flex-col justify-start items-center">
+                        <View className="w-[25px] p-[3px] mt-[7px] rounded-full flex justify-center items-center">
+                            <FontAwesome name="circle-o" size={20} color="#126FD7" />
+                        </View>
+                    </View>
+                    <View className="flex flex-row justify-between border-[#EFEFEF] border-b-[1.5px]">
+                        <View className="flex flex-col justify-center w-[240px] max-w-[262px]">
+                            <Text className='text-[16px] font-bold'>Huỳnh Sơn</Text>
+                            <Text className='text-[13px]'>5.34km . Pick up/Drop off Gate, 227 Nguyễn</Text>
+                        </View>
+                        <View className='w-[19px] flex flex-col justify-center items-center pt-[10px] ml-[20px]'>
+                            <Ionicons name="bookmark-outline" size={17} color="black" />
+                        </View>
+                    </View>
+                </View>
+                <View className="flex flex-row items-center">
+                    <Text>Add pick-up notes for driver</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
