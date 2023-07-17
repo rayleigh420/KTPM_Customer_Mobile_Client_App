0e21a69ad573608fe336ffa5c297ae81b257d96d
rayleigh420 <nhatduy0409@gmail.com>
2023-07-17 20:10:39 +0700

    Create PickUp components

--- app/find/map.js
+++ app/find/map.js
@@ -1,22 +1,18 @@
 import { View, Text, Pressable, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import { Stack } from 'expo-router'
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Entypo, Feather, FontAwesome5, FontAwesome } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 import ViewMap from '../../src/components/Map/ViewMap';
+import PickUp from '../../src/components/Location/PickUp';
 
 export default function Map() {
-    const [addPickUp, setAddPickUp] = useState(false)
+    const [pickBook, setPickBook] = useState(false);
 
     return (
         <View className="h-full relative">
             <Stack.Screen options={{
                 title: "",
                 headerShadowVisible: false,
-                // headerStyle: {
-                //     // backgroundColor: 'transparent',
-                //     backgroundColor: 'transparent'
-                // },
                 headerTransparent: true,
 
                 headerLeft: () => {
@@ -28,47 +24,13 @@ export default function Map() {
 
                 }
             }} />
-            {/* <Text className='absolute'>Hello</Text> */}
             <ViewMap className="" />
-            <View className="absolute bottom-0 w-full px-[15px] flex flex-col justify-center items-center bg-white pt-[20px]">
-                <View className="bg-[#F2F2F2] flex flex-row justify-between items-center px-[17px] py-[15px] min-w-[350px] rounded-[5px]">
-                    <View className="flex flex-col justify-start items-center">
-                        <View className="w-[25px] p-[3px] mt-[7px] rounded-full flex justify-center items-center">
-                            <FontAwesome name="circle-o" size={20} color="#126FD7" />
-                        </View>
-                    </View>
-                    <View className="flex flex-row justify-between border-[#EFEFEF] border-b-[1.5px]">
-                        <View className="flex flex-col justify-center w-[240px] max-w-[262px]">
-                            <Text className='text-[16px] font-bold'>Huỳnh Sơn</Text>
-                            <Text className='text-[13px]'>5.34km . Pick up/Drop off Gate, 227 Nguyễn</Text>
-                        </View>
-                        <View className='w-[19px] flex flex-col justify-center items-center pt-[10px] ml-[20px]'>
-                            <Ionicons name="bookmark-outline" size={17} color="black" />
-                        </View>
-                    </View>
-                </View>
-                {
-                    !addPickUp ?
-                        (
-                            <Pressable onPress={() => setAddPickUp(true)} className="flex flex-row items-center justify-start min-w-[350px] pl-[15px] py-[20px] border-[#EFEFEF] border-b-[1.5px] mb-[20px]">
-                                <Entypo name="edit" size={24} color="#01B0E7" />
-                                <Text className="text-[#01B0E7] ml-[13px] font-bold">Add pick-up notes for driver</Text>
-                            </Pressable>
-                        )
-                        :
-                        (
-                            <View className="min-w-[350px] border-[#EFEFEF] border-b-[1.5px] mb-[20px]">
-                                <View className="flex flex-row min-w-[350px] pl-[15px] py-[10px] mt-[20px] rounded-[8px] border-[#2EAF69] border-[1.5px] mb-[20px]">
-                                    <Entypo name="edit" size={24} color="gray" />
-                                    <TextInput placeholder='Add pick-up notes for driver' className="font-normal ml-[10px] min-w-[300px]" />
-                                </View>
-                            </View>
-                        )
-                }
-                <View className="bg-[#00B14F] min-w-[350px] py-[15px] rounded-[8px] flex justify-center items-center mb-[15px]">
-                    <Text className="text-white font-bold text-[18px]">Choose this Pick-Up</Text>
-                </View>
-            </View>
+            {
+                !pickBook ?
+                    <PickUp setPickBook={setPickBook} />
+                    :
+                    <Text>Hello</Text>
+            }
         </View>
     )
 }
